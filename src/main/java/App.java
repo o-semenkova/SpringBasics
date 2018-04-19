53141a2e8c77d01d6db41ec605b0a8c3d368c9b4
Oksana Semenkova <osemenkova@intellias.com>
2018-04-19 18:07:48 +0300

    CombinedEventLogger was added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,21 +5,34 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 public class App {
+    private ConsoleEventLogger consoleEventLogger;
+    private CombinedEventLogger combinedEventLogger;
     private Client client;
     private CacheFileEventLogger cacheFileEventLogger;
 
     public App() {
     }
 
-    public App(Client client, CacheFileEventLogger cacheFileEventLogger) {
+    public App(Client client, CacheFileEventLogger cacheFileEventLogger,
+               CombinedEventLogger combinedEventLogger,
+               ConsoleEventLogger consoleEventLogger) {
         this.client = client;
         this.cacheFileEventLogger = cacheFileEventLogger;
+        this.combinedEventLogger = combinedEventLogger;
+        this.consoleEventLogger = consoleEventLogger;
     }
 
-    private void logEvent(Event event) throws IOException {
+    private void logEvent(Event event, EventType eventType) throws IOException {
         String message = event.getMsg().replaceAll(client.getId().toString(), client.getFullName());
         event.setMsg(message);
-        cacheFileEventLogger.logEvent(event);
+
+        if(eventType == null){
+            cacheFileEventLogger.logEvent(event);
+        }else if(eventType.equals(EventType.INFO)){
+            consoleEventLogger.logEvent(event);
+        }else if(eventType.equals(EventType.ERROR)){
+            combinedEventLogger.logEvent(event);
+        }
     }
 
     public static void main(String args[]) throws IOException {
@@ -27,9 +40,11 @@ public class App {
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App) ctx.getBean("app");
         Event event_1 = (Event)ctx.getBean("event");
-        app.logEvent(event_1);
+        app.logEvent(event_1, EventType.ERROR);
         Event event_2 = (Event)ctx.getBean("event");
-        app.logEvent(event_2);
+        app.logEvent(event_2, EventType.INFO);
+        Event event_3 = (Event)ctx.getBean("event");
+        app.logEvent(event_3, null);
         ctx.close();
 
     }
